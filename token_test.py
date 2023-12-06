561e348216a7a54484cc54cc56c37c711c8da18a
ivsitum <77884790+ivsitum@users.noreply.github.com>
2023-12-06 13:04:17 +0100

    Create token_test.py

---
+++ token_test.py
@@ -0,0 +1,7 @@
+def main():
+    # Dummy API token for testing purposes
+    dummy_token = "1234567890abcdef1234567890abcdef"
+    print(f"Dummy API Token for testing: {dummy_token}")
+
+if __name__ == "__main__":
+    main()
