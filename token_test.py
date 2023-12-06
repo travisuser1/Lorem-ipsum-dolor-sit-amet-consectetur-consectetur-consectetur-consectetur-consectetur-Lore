f980a73f673951ae1600b04da20085fd86ffa7e3
ivsitum <77884790+ivsitum@users.noreply.github.com>
2023-12-06 13:09:19 +0100

    Update token_test.py

--- token_test.py
+++ token_test.py
@@ -1,7 +1,8 @@
 def main():
-    # Dummy API token for testing purposes
+    # Explicitly printing a dummy API token for Travis CI log scan testing purposes
     dummy_token = "1234567890abcdef1234567890abcdef"
-    print(f"Dummy API Token for testing: {dummy_token}")
+    print("Testing Travis CI log scan for API token detection:")
+    print("Dummy API Token:", dummy_token)
 
 if __name__ == "__main__":
     main()
